b6d15cbb0151cbd67fb234a9f3b4e79c02b7cafb
Antonio Linares <antonio.fivetech@gmail.com>
2015-02-03 10:40:56 +0100

    * Batch to set enviroment settings for Windows from Francesco

---
+++ set_env.bat
@@ -0,0 +1,6 @@
+PATH=c:\qt\tools\mingw491_32\bin;C:\qt\5.4\mingw491_32\bin;C:\harbour\bin\
+SET QT_PLUGIN_PATH=C:\Qt\5.4\mingw491_32\plugins
+SET HB_WITH_QT=C:\qt\5.4\mingw491_32\include
+rem HB_QT_MAJOR_VER can probably become deprecated in next
+rem Harbour/qtcontribs releases.
+SET HB_QT_MAJOR_VER=5
