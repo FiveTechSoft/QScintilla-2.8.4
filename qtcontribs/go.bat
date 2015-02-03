b97e1c4368c2913c45b7540716e30b77807f3610
Antonio Linares <antonio.fivetech@gmail.com>
2015-02-03 13:34:34 +0100

    + Added support for SendScintilla( nMsg, nWParam, nLParam )
    * set_env.bat turned into go.bat to build the Windows version

--- qtcontribs/go.bat
+++ qtcontribs/go.bat
@@ -4,3 +4,4 @@ SET HB_WITH_QT=C:\qt\5.4\mingw491_32\include
 rem HB_QT_MAJOR_VER can probably become deprecated in next
 rem Harbour/qtcontribs releases.
 SET HB_QT_MAJOR_VER=5
+hbmk2 qtcontribs.hbp
